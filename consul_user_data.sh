432d51a1474b1845bde53fe2a49bcdfe445a4856
sm_peterson@yahoo.com <gi6h259b>
2016-10-31 08:25:41 -0500

    11

--- consul_user_data.sh
+++ consul_user_data.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -x
+exec > >(tee /var/log/user-data.log|logger -t user-data ) 2>&1
+echo BEGIN
+date '+%Y-%m-%d %H:%M:%S'
 sudo apt-get update
 sudo apt-get install -y curl wget unzip python-pip nmon
 sudo pip install awscli
@@ -9,12 +13,9 @@ unzip consul.zip >/dev/null
 chmod +x consul
 sudo mv consul /usr/local/bin/consul
 sudo mkdir -p /opt/consul/data
-sudo aws s3 cp s3://openshifts3bucket/consul-server-count /tmp/consul-server-count
-sudo aws s3 cp s3://openshifts3bucket/consul-server-addr /tmp/consul-server-addr
 sudo aws s3 cp s3://openshifts3bucket/upstart.conf /tmp/upstart.conf
 sudo aws s3 cp s3://openshifts3bucket/consul_flags /tmp/consul_flags
-SERVER_COUNT=$(cat /tmp/consul-server-count | tr -d '\n')
-CONSUL_JOIN=$(cat /tmp/consul-server-addr | tr -d '\n')
+sudo aws s3 cp s3://openshifts3bucket/firststart /tmp/firststart
 sudo mkdir -p /etc/consul.d
 sudo mkdir -p /etc/service
 sudo chown root:root /tmp/upstart.conf
@@ -22,4 +23,5 @@ sudo mv /tmp/upstart.conf /etc/init/consul.conf
 sudo chmod 0644 /etc/init/consul.conf
 sudo mv /tmp/consul_flags /etc/service/consul
 sudo chmod 0644 /etc/service/consul
-sudo start consul
+sudo chmod 0777 /tmp/firststart
+sudo /tmp/firststart
--- standards.tf
+++ standards.tf
@@ -2,3 +2,7 @@ variable "ami_standard" {
 	default = "ami-2051294a"
 }
 
+variable "consul_servers" {
+    default = "2"
+    description = "The number of Consul servers to launch."
+}
